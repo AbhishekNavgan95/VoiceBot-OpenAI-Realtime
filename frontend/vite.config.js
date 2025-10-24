58349b5ec3c372ebe47afbdb5c69bd8cb34cbd5f
SomvanshiTechnologies <somvanshitech.pune@gmail.com>
2025-10-24 16:20:55 +0530

    chore: Update Netlify configuration and clean up Vite settings
    - Added build and header configurations in netlify.toml for improved security and proper content handling.
    - Removed unnecessary server configuration from vite.config.js to streamline the setup.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://26dc94a9cb34.ngrok-free.app',
-        changeOrigin: true,
-      }
-    }
-  }
+  plugins: [react()]
 })
